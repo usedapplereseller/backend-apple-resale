b4dcc7457c55f20f9ca66621ccefc11d403b376f
yuntingc <chiayunting@gmail.com>
2022-10-27 23:49:34 +0800

    edited product down seeding function

--- db/seeders/20220614200905-seed-users-and-listings.js
+++ db/seeders/20220614200905-seed-users-and-listings.js
@@ -69,5 +69,6 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete("listings", null, {});
     await queryInterface.bulkDelete("users", null, {});
+    await queryInterface.bulkDelete("products", null, {});
   },
 };
